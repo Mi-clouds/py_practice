2ce44e6daa363e854bf706e838c25fd11ac4cf13
Michelle <mistrymichelle@gmail.com>
2021-02-13 15:49:32 +0000

    commit

--- list.py
+++ list.py
@@ -3,3 +3,9 @@ Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,F
 print(int(len(Belgium)) * "-")
 print(Belgium.replace(",", ";"))
 
+Belgium_list = Belgium.split(",")
+print(Belgium_list)
+
+Belguim_pop = int(Belgium_list[1])
+Brussels_pop = int(Belgium_list[3])
+print(Belguim_pop + Brussels_pop)
